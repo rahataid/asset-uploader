ffae80cb4d334d289e28e1d1b8f2f7e6b3998eb2
bipinparajuli <bipinprjl@gmail.com>
2024-05-23 14:57:20 +0545

    fix: function to handle filename

--- src/upload-handler.ts
+++ src/upload-handler.ts
@@ -2,7 +2,7 @@ import {
   AssetUploaderInstances,
   AssetAvailableUploaders,
   UploadAssetParams,
-} from "./types";
+} from './types';
 
 class AssetUploader {
   private static instances: AssetUploaderInstances | any = {};
@@ -12,7 +12,7 @@ class AssetUploader {
     this.uploaderName = name;
     switch (name) {
       case AssetAvailableUploaders.S3:
-        const S3Uploader = require("./aws").default;
+        const S3Uploader = require('./aws').default;
         AssetUploader.instances[name] = new S3Uploader(config);
         break;
       // Add more cases for other uploaderNames here
@@ -27,6 +27,17 @@ class AssetUploader {
     }
 
     return AssetUploader.instances[this.uploaderName].uploadFile(uploadParams);
+  }
+  static async uploadWithFileName(
+    uploadParams: UploadAssetParams
+  ): Promise<string> {
+    if (!AssetUploader.instances[this.uploaderName]) {
+      throw new Error(`Uploader instance '${this.uploaderName}' not found`);
+    }
+
+    return AssetUploader.instances[this.uploaderName].uploadWithFileName(
+      uploadParams
+    );
   }
 }
 
